bf417abce1e7711a18f0b3bc98c1c1b8e110f2e3
lrnzpn <migzpinaroc@gmail.com>
2020-02-22 17:52:17 +0800

    Fixed navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,20 +2,28 @@
   <div id="sidenav">
     <div class="nav-menu">
       <router-link to="/">
-        <img src="../assets/icons/dashboard.png" alt="" />
-        <span>Dashboard</span>
+        <div class="nav-link">
+          <img src="../assets/icons/dashboard.png" alt="" />
+          <span>Dashboard</span>
+        </div>
       </router-link>
       <router-link to="/settings">
-        <img src="../assets/icons/settings.png" alt="" />
-        <span>Settings</span>
+        <div class="nav-link">
+          <img src="../assets/icons/settings.png" alt="" />
+          <span>Settings</span>
+        </div>
       </router-link>
       <router-link to="/import">
-        <img src="../assets/icons/input.png" alt="" />
-        <span>Import</span>
+        <div class="nav-link">
+          <img src="../assets/icons/input.png" alt="" />
+          <span>Import</span>
+        </div>
       </router-link>
       <router-link to="/">
-        <img src="../assets/icons/logout.png" alt="" />
-        <span>Log out</span>
+        <div class="nav-link">
+          <img src="../assets/icons/logout.png" alt="" />
+          <span>Log out</span>
+        </div>
       </router-link>
     </div>
   </div>
@@ -40,21 +48,28 @@ export default {};
 
 .nav-menu {
   height: 100%;
-  padding: 7.5rem 20%;
+  padding: 7.5rem 0;
   display: flex;
   justify-content: flex-start;
   align-items: center;
   flex-direction: column;
 
   a {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
     color: $white;
     width: 100%;
     margin-bottom: 20px;
-    padding: 0.75em;
     border-radius: 10px;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    .nav-link {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      margin: 0.5em 1em;
+    }
   }
 
   img {
